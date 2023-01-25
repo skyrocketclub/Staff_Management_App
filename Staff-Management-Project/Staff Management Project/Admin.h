7b4131f173d16fbc1eb3b6f0ed8117d65b8cdfa4
Charles <charliecharleston1121@gmail.com>
2023-01-25 10:13:41 +0100

    Done with the two tasks

--- Staff-Management-Project/Staff Management Project/Admin.h
+++ Staff-Management-Project/Staff Management Project/Admin.h
@@ -44,7 +44,8 @@ public:
     bool addAdmin();
 
 private:
-    const char* dir = "c:\\users\\user\\desktop\\Staff Management Project\\office.db";
+   // const char* dir = "c:\\users\\user\\desktop\\Staff Management Project\\office.db";
+    const char* dir = "C:\\Users\\charles\\source\\repos\\Staff_Management_App"
     string verifyPassword(string);
 
 };
